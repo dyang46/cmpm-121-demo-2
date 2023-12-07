a97083dc1a5d7f49bfabbb8c74d18fa93db6885e
dyang46 <103404171+dyang46@users.noreply.github.com>
2023-12-07 00:51:09 -0800

    Step 1 Completed

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,7 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
-app.append(header);
+
+const canvas = document.createElement("canvas");
+
+app.append(header, canvas);
